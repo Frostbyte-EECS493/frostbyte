9124c1411fb88f51f1fdc032a8abe757e682e9b2
edpeper <edpeper@umich.edu>
2020-04-06 17:32:26 -0400

    query comments section in API still pending fix

--- script.js
+++ script.js
@@ -3,13 +3,27 @@ var resultView = new Vue({
   el: '#app',
   data: {
     resultData: [],
-    display: true //modify this if needed next time
+    display: true, //modify this if needed next time,
+    userNameSearch: '',
+    userSearchData: []
   },
   methods: {
     mounted: function() {
     	var firebaseRef = firebase.storage().ref()
     },
+  setComment: function() {
+    let firebaseRefPosts = firebase.database().ref("posts")
+    firebaseRefPosts.orderByChild("postId").equalTo(1).once('value')
+		// update post count
+		.then( (snap) => {
+			firebaseRefPosts.set(snap.likes.val() + 1)
+  		})
+  },
+  likePost: function(){
+
+  },
     setUploadImg: function() { 
+	this.resultData = []
   	console.log("choosing");
         var pic = document.getElementById("setUploadImg"); 
   
@@ -24,55 +38,77 @@ var resultView = new Vue({
         // Save to firebase storage
         resultView.uploadImg();
     },
+    //This function retrieves user's photos from database based on username (if exists)
+    viewOwnerImgs: function() {
+      let term = this.userNameSearch + '/photos';
+
+      firebase.database().ref(term).once('value').then(function(snapshot) {
+        let returnArr = [];
+        snapshot.forEach(function(childSnapshot) {
+          returnArr.push(childSnapshot.val())
+          //this.userSearchData.push(childSnapshot.val())
+          // this.userSearchData.push(childSnapshot.val());
+        });
+        if(!returnArr.length) {
+          alert("Please enter a valid username.");
+          return;
+        }
+      resultView.userSearchData = returnArr;
+      console.log("Youve reached it");
+      console.log(resultView.userSearchData);
+
+      });
+    },
     uploadImg: function() {
-	// references to database objects
-	let firebaseRefPosts = firebase.database().ref("posts")
-	let firebaseRefPostCount = firebase.database().ref("postCount")
+      // references to database objects
+      let firebaseRefPosts = firebase.database().ref("posts")
+      let firebaseRefPostCount = firebase.database().ref("postCount")
 
      	let name = Date.now();
      	console.log("Uploading at " + name);
      	let storageRef = firebase.storage().ref('/images/'+ name);
      	let uploadTask = storageRef.put(selectedFile);
 
-     	uploadTask.on('state_changed', function(snapshot){ 
-     		/**
-            var progress =  
-             (snapshot.bytesTransferred / snapshot.totalBytes) * 100; 
-              var uploader = document.getElementById('uploader'); 
-              uploader.value=progress; 
-              switch (snapshot.state) { 
-                case firebase.storage.TaskState.PAUSED: 
-                  console.log('Upload is paused'); 
-                  break; 
-                case firebase.storage.TaskState.RUNNING: 
-                  console.log('Upload is running'); 
-                  break; 
-              } **/
-        });
+    
         // add new post info to firebase database
         uploadTask.then( () => {
+		let currentPostId = 0
+  		firebaseRefPostCount.once('value')
 		// update post count
-  		firebaseRefPostCount.once('value').then(function(snapshot) {
-			firebaseRefPostCount.set(snapshot.val() + 1)
+		.then( (snap) => {
+			currentPostId = snap.val()
+			firebaseRefPostCount.set(snap.val() + 1)
   		})
 		// initialize new post
-		let newPostRef = firebaseRefPosts.push()
-		storageRef.getDownloadURL().then( function(url) {
-			newPostRef.set({
-				'imgUrl': url,
-				'likes': 0,
-				'comments': '' 
+		.then( () => {
+			console.log("create new empty post id " + currentPostId)
+			let newPostRef = firebaseRefPosts.push()
+			storageRef.getDownloadURL().then( (url) => {
+				newPostRef.set({
+					'imgUrl': url,
+					'likes': 0,
+					'comments': '',
+					'postId': currentPostId
+				})
 			})
 		})
 		// load all images from updated firebase database
-		firebaseRefPosts.once('value').then( (snapshot) => {
-			snapshot.forEach( (childSnapshot) => {
-				//let childKey = childSnapshot.key
-				//let childData = childSnapshot.val()
-				this.resultData.push(childSnapshot.val())
-			})
-  		})
-	});	
-     }
-  }
+		.then( () => {
+			console.log("loading images from database")
+			firebaseRefPosts.on('value', (snapshot) => {
+				snapshot.forEach( (childSnapshot) => {
+					//let childKey = childSnapshot.key
+					//let childData = childSnapshot.val()
+					console.log("loading...")
+					console.log(childSnapshot.val())
+					this.resultData.push(childSnapshot.val())
+				})
+  			})
+			console.log("this.resultData values:")
+			console.log(this.resultData)
+		})
+       })	
+      
+    }
+  }, //end of methods
 })
