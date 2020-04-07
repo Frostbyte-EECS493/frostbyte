50626272eb8a4ef85da6d031bde354819a31a330
ckairen <albert4510@gmail.com>
2020-04-06 22:47:21 -0400

    fixed duplicate images

--- script.js
+++ script.js
@@ -14,14 +14,14 @@ var resultView = new Vue({
     	var firebaseRef = firebase.storage().ref()
     },
     setUploadImg: function() { 
-	this.resultData = []
-  	console.log("choosing");
-        var pic = document.getElementById("setUploadImg"); 
+	    this.resultData = []
+  	  console.log("choosing");
+      var pic = document.getElementById("setUploadImg"); 
   
-        // selected file is that file which user chosen by html form 
-        selectedFile = pic.files[0];
-        console.log("File")
-        console.log(selectedFile)
+      // selected file is that file which user chosen by html form 
+      selectedFile = pic.files[0];
+      console.log("File")
+      console.log(selectedFile)
   
         // Make save button disabled for few seconds that has id='submitImg'
         // Avoid spam saving
@@ -33,7 +33,8 @@ var resultView = new Vue({
     viewOwnerImgs: function() {
       let term = this.userNameSearch + '/photos';
 
-      firebase.database().ref(term).once('value').then(function(snapshot) {
+      firebase.database().ref(term).once('value')
+      .then(function(snapshot) {
         let returnArr = [];
         snapshot.forEach(function(childSnapshot) {
           returnArr.push(childSnapshot.val())
@@ -44,10 +45,9 @@ var resultView = new Vue({
           alert("Please enter a valid username.");
           return;
         }
-      resultView.userSearchData = returnArr;
-      console.log("Youve reached it");
-      console.log(resultView.userSearchData);
-
+        resultView.userSearchData = returnArr;
+        console.log("Youve reached it");
+        console.log(resultView.userSearchData);
       });
     },
     uploadImg: function() {
@@ -62,44 +62,49 @@ var resultView = new Vue({
 
     
         // add new post info to firebase database
-        uploadTask.then( () => {
-		let currentPostId = 0
-  		firebaseRefPostCount.once('value')
-		// update post count
-		.then( (snap) => {
-			currentPostId = snap.val()
-			firebaseRefPostCount.set(snap.val() + 1)
-  		})
-		// initialize new post
-		.then( () => {
-			console.log("create new empty post id " + currentPostId)
-			let newPostRef = firebaseRefPosts.push()
-			storageRef.getDownloadURL().then( (url) => {
-				newPostRef.set({
-					'imgUrl': url,
-					'likes': 0,
-					'comments': '',
-					'postId': currentPostId
-				})
-			})
-		})
-		// load all images from updated firebase database
-		.then( () => {
-			console.log("loading images from database")
-			firebaseRefPosts.on('value', (snapshot) => {
-				snapshot.forEach( (childSnapshot) => {
-					//let childKey = childSnapshot.key
-					//let childData = childSnapshot.val()
-					console.log("loading...")
-					console.log(childSnapshot.val())
-					this.resultData.push(childSnapshot.val())
-				})
-  			})
-			console.log("this.resultData values:")
-			console.log(this.resultData)
-		})
-       })	
-      
+      uploadTask.then( () => {
+		    let currentPostId = 0
+  		  firebaseRefPostCount.once('value')
+		    // update post count
+		    .then( (snap) => {
+			    currentPostId = snap.val()
+			    firebaseRefPostCount.set(snap.val() + 1)
+  		  })
+		    // initialize new post
+		    .then( () => {
+			    console.log("create new empty post id " + currentPostId)
+			    let newPostRef = firebaseRefPosts.push()
+			    storageRef.getDownloadURL()
+          .then( (url) => {
+				    newPostRef.set({
+				    	'imgUrl': url,
+				    	'likes': 0,
+				    	'comments': '',
+				    	'postId': currentPostId
+				    }, function(error) {
+              if (error) {
+                console.log(error)
+              } else {
+                // Data saved successfully!
+                console.log("loading images from database")
+                firebaseRefPosts.once('value')
+                .then((snapshot) => {
+                  snapshot.forEach( (childSnapshot) => {
+                    //let childKey = childSnapshot.key
+                    //let childData = childSnapshot.val()
+                    console.log("loading...")
+                    console.log(childSnapshot.val())
+                    resultView.resultData.push(childSnapshot.val())
+                  })
+                })
+                console.log("this.resultData values:")
+                console.log(resultView.resultData)
+                document.getElementById('submitImg').removeAttribute('disabled');
+              }
+			      })
+		      })
+        })
+      })
     }
   }, //end of methods
 })
